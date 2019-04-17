7e6ba936388c11ac83b13dee64ed85954a757f80
ReturnTears <returntears@outlook.com>
2019-04-17 23:27:28 +0800

    SpringBoot整合文件的上传下载

---
+++ src/main/java/allst/boot/strap/controller/AllstVueEcharts.java
@@ -0,0 +1,18 @@
+package allst.boot.strap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author June 2019/04/04 下午 08:03
+ * @version 1.0
+ */
+@Controller
+@RequestMapping("/echarts")
+public class AllstVueEcharts {
+
+    @RequestMapping("/1")
+    public String vuecharts1() {
+        return "echarts/Vuecharts1";
+    }
+}
